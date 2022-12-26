4d1d42befb9208d82d527b2911cc8f6286794f8c
a22106 <bk22106@gmail.com>
2022-12-27 01:29:10 +0900

    update v2x 2208

--- v2x.py
+++ v2x.py
@@ -51,8 +51,10 @@ class V2XData():
         else: 
             self.hazard_thr = hazard_thr
         
-        print(f"loaded {len(self.dataset_scenario)} files")
-        print(self.dataset_scenario[:5], "...")
+        if self.files_num:
+            print(f"loaded {len(self.dataset_scenario)} files")
+            print(self.dataset_scenario[:5], "...")
+            
         
     def __getitem__(self, index, print_size=True):
         df = pd.read_csv(self.dataset_scenario[index]).drop(labels = self.drop_cols, axis=1)
@@ -115,25 +117,46 @@ class V2XData():
     
 DROP_COLS_L = ['ISSUE_DATE', 'VEHICLE_ID', 'VEHICLE_TYPE']
 
-class V2XDataLabeled(V2XData):
-    def __init__(self, dataset_path = None, dataset_scenario: list = None, drop_cols: list = None, 
+# class V2XDataLabeled(V2XData):
+#     def __init__(self, dataset_path = None, dataset_scenario: list = None, drop_cols: list = None, 
+#                  target_cols: list = None, test_size: float = 0.5, random_state: int = None):
+#         super().__init__(dataset_scenario, drop_cols, target_cols, test_size, random_state)
+
+class V2XDataLabeled():
+    def __init__(self, dataset_path, condition: str, dataset_scenario: list = None, drop_cols: list = None, 
                  target_cols: list = None, test_size: float = 0.5, random_state: int = None):
-        super().__init__(dataset_path, dataset_scenario, drop_cols, target_cols, test_size, random_state)
+        self.condition = condition
+        self.scenario_path = os.path.join(dataset_path, condition)
+        
         if dataset_scenario:
             self.dataset_scenario = dataset_scenario.copy()
         else:
-            self.dataset_scenario = sorted(glob.glob(os.path.join(self.data_path, '*')))
+            self.dataset_scenario = sorted(glob.glob(os.path.join(self.scenario_path, '*')))
         self.dataset_scenario = [file for file in self.dataset_scenario if os.path.isdir(file)]
         
-        self.drop_cols = DROP_COLS_L.copy()
+        if not drop_cols:
+            self.drop_cols = DROP_COLS_L.copy()
+        else:
+            self.drop_cols = drop_cols.copy()
+            
+        if target_cols:
+            self.target_cols = target_cols.copy()
+        else:
+            self.target_cols = None
+            
+        self.test_size = test_size
+        self.random_state = random_state
+        
         
         print(f"loaded {len(self.dataset_scenario)} scenarios")
         print(self.dataset_scenario[:5], "...")
             
+    def __len__(self):
+        return len(self.dataset_scenario)
         
     def __getitem__(self, index, print_size=True):
         csv_files = sorted(glob.glob(os.path.join(self.dataset_scenario[index], '*.csv')))
-        json_files = sorted(glob.glob(os.path.join(self.dataset_scenario[index], '*.json')))
+        json_files = sorted(glob.glob(os.path.join(self.dataset_scenario[index].replace("/C", ""), '*.json')))
         
         df = pd.DataFrame()
         X = []
@@ -146,17 +169,25 @@ class V2XDataLabeled(V2XData):
         
         df.fillna(0, inplace = True)
         
-        y = []
-        for json_file in json_files:
-            with open(json_file, 'r') as f:
-                json_data = json.load(f)
-                y.append(json_data["Annotation"]['Turn'])
-        
-        y = np.array(y)
-        
+        if json_files:
+            y = []
+            for json_file in json_files:
+                with open(json_file, 'r') as f:
+                    json_data = json.load(f)
+                    y.append([json_data["Annotation"]['Turn'], json_data["Annotation"]['Lane'], json_data["Annotation"]["Speed"], json_data["Annotation"]['Hazard']])
+            
+            y = np.array(y)
+        else:
+            y = np.zeros((len(X), 4))
+        print(X, self.test_size, self.random_state)
         splits = self.get_splits(X, test_size = self.test_size, random_state = self.random_state)
         
         return X, y, splits, df
+    
+    def get_splits(self, X, test_size: float = 0.5, random_state: int = None):
+        X_train, X_valid = train_test_split(X, test_size=test_size, random_state=random_state)
+        splits = get_predefined_splits(X_train, X_valid)
+        return splits
     
     def get_all_item(self, is_test = False, print_size=True):
         X_sum, y_sum = [], []
@@ -167,7 +198,7 @@ class V2XDataLabeled(V2XData):
         print(f'files index: 0 ~ {files_num}')
         
         for idx in tqdm(range(files_num)):
-            X, y, _, df = self.__getitem__(idx, print_size=False)
+            X, y, _, df = self.__getitem__(idx, print_size)
             X_sum.append(X)
             y_sum.append(y)
             df_sum = pd.concat([df_sum, df])
